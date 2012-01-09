8d758690fd26671663a18095cef9a043385d15bc
George Williams <george.williams@gmail.com>
2012-01-09 03:09:10 -0800

    getvideos

--- website/api/server.py
+++ website/api/server.py
@@ -78,6 +78,64 @@ class GetHomeDetails( resource.Resource ):
 		retv = json.dumps( heroes )
 		return retv
 
+#
+# Call to get search results...
+#
+class GetVideos(resource.Resource):
+
+        def render_GET( self, request ):
+		search1 = { 'thumb':'http://www.yellowdragon-music.co.uk/images/miami/Danny_Tenaglia_M.jpg', \
+				'video':'http://www.youtube.com/watch?v=ioJa7UvCqEA&', \
+				'rating':4, \
+				'name':'Tenaglia Video 1' }
+		search2 = { 'thumb':'http://www.houseplanet.dj/portal_images/beatport-artists/beatport-danny-tenaglia.jpg',\
+				'video':'http://www.youtube.com/watch?v=IAtSFEbn_I0', \
+				'rating':3, \
+				'name':'Tenaglia Video 2' }
+		search3 = { 'thumb':'http://www.electronicmusic.cc/images/danny-tenaglia-carl-cox.jpg', \
+				'video':'http://www.youtube.com/watch?v=AhDSbmiFkSg',\
+				'rating':3, \
+				'name':'Tenaglia Video 3' }
+		search4 = { 'thumb':'http://www.djdownload.com/earworm/wp-content/uploads/dannytenaglia.jpg', \
+				'video':'http://www.youtube.com/watch?v=K_6c4XnUggE', \
+				'rating':2, \
+				 'name':'Tenaglia Video 4' }
+		search5 = { 'thumb':'http://www.poslouchej.net/forum/userpix/238_Danny_Tenaglia__Global_Underground_010_Athens_1.jpg', \
+				'video':'http://www.youtube.com/watch?v=H0vwthmnpqQ', \
+				'rating':1, \
+				 'name':'Tenaglia Video 5' }
+		search6 = { 'thumb':'http://www.djsets.co.uk/Compilations/dannytanaglia/dan3.jpg', \
+				'video':'http://www.youtube.com/watch?v=GPqD_bpA72U', \
+				'rating':3, \
+				 'name':'Tenaglia Video 6' }
+		search7 = { 'thumb':'http://www.beatportal.com/uploads/news/1211524469_danny-tenaglia442.jpg', \
+				'video':'http://www.youtube.com/watch?v=cWI8Mmws7Xg', \
+				'rating':4, \
+				 'name':'Tenaglia Video 7' }
+		search8 = { 'thumb':'http://www.yellowdragon-music.co.uk/images/miami/Danny_Tenaglia_M.jpg', \
+				'video':'http://www.youtube.com/watch?v=ioJa7UvCqEA&', \
+				'rating':4, \
+				 'name':'Tenaglia Video 8' }
+		search9 = { 'thumb':'http://www.electronicmusic.cc/images/danny-tenaglia-carl-cox.jpg', \
+				'video':'http://www.youtube.com/watch?v=AhDSbmiFkSg',\
+				'rating':3, \
+				 'name':'Tenaglia Video 9' }
+		search10 = { 'thumb':'http://www.djdownload.com/earworm/wp-content/uploads/dannytenaglia.jpg', \
+				'video':'http://www.youtube.com/watch?v=K_6c4XnUggE', \
+				'rating':2, \
+				 'name':'Tenaglia Video 10' }
+		search11 = { 'thumb':'http://www.poslouchej.net/forum/userpix/238_Danny_Tenaglia__Global_Underground_010_Athens_1.jpg', \
+				'video':'http://www.youtube.com/watch?v=H0vwthmnpqQ', \
+				'rating':4, \
+				 'name':'Tenaglia Video 11' }
+		search12 = { 'thumb':'http://www.poslouchej.net/forum/userpix/238_Danny_Tenaglia__Global_Underground_010_Athens_1.jpg', \
+				'video':'http://www.youtube.com/watch?v=H0vwthmnpqQ', \
+				'rating':1, \
+				 'name':'Tenaglia Video 12' }
+		results = [ search1, search2, search3, search4, search5, search6, search7, search8, search9, search10, search11, search12 ]
+		retstr = json.dumps( results )
+		return retstr
+
 # API top level resource...
 class API(resource.Resource):
 
@@ -87,6 +145,9 @@ class API(resource.Resource):
 
 		elif path=="get_home_details":
 			return GetHomeDetails()	
+
+		elif path=="get_videos":
+			return GetVideos()
 
 a = API()
 parent.putChild("api", a)
