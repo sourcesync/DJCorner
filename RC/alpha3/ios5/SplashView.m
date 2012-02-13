a735de5567da7e5f8f1118ca6456cfb5c35ca491
Leve <ltan@zixmedia.com>
2012-02-13 17:40:27 +0800

    video and background music

--- RC/alpha3/ios5/SplashView.m
+++ RC/alpha3/ios5/SplashView.m
@@ -11,6 +11,7 @@
 
 @implementation SplashView
 
+@synthesize player;
 @synthesize img=_img;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -23,14 +24,15 @@
     return self;
 } 
 
--(id)init
-{
-    self = [ self initWithNibName:@"SplashView" bundle:nil ];
-    return self;
-}
+//-(id)init
+//{
+//    self = [ self initWithNibName:@"SplashView" bundle:nil ];
+//    return self;
+//}
 
 - (void)dealloc
 {
+    
     [super dealloc];
 }
 
@@ -54,13 +56,78 @@
     self.img.backgroundColor = [ UIColor clearColor ];
     
     [[UIApplication sharedApplication] setStatusBarHidden:YES animated:NO];
+    
+    
+}
+-(BOOL) prepAudio
+{
+	NSError *error;
+	NSString *path = [[NSBundle mainBundle] pathForResource:@"MeetMeInSt.Louis1904" ofType:@"mp3"];
+	if (![[NSFileManager defaultManager] fileExistsAtPath:path]) return NO;
+	
+	// Initialize the player
+	self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:path] error:&error];
+	self.player.delegate = self;
+    player.numberOfLoops = -1;  
+	if (!self.player)
+	{
+		NSLog(@"Error: %@", [error localizedDescription]);
+		return NO;
+	} 
+	[self.player prepareToPlay];
+    
+	return YES;
 }
 
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+	// just keep playing
+	[self.player play];
+}
+
+-(void)playMovieAtURL:(NSURL*)theURL
+{
+    theMovie= [[MPMoviePlayerController alloc] initWithContentURL:theURL];
+    if (theMovie) {
+        
+        theMovie.scalingMode=MPMovieScalingModeAspectFill;
+        //theMovie.userCanShowTransportControls=NO; 
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(myMovieFinishedCallback:)
+                                                     name:MPMoviePlayerPlaybackDidFinishNotification
+                                                   object:theMovie];
+        [[theMovie view] setFrame:CGRectMake(0, 0, 320, 460)]; 
+        // Movie playback is asynchronous, so this method returns immediately. 
+        [[self view] addSubview:[theMovie view]]; 
+        [theMovie play];
+    }
+}
+
+// When the movie is done,release the controller.
+- (void)myMovieFinishedCallback:(NSNotification*)aNotification
+{
+    theMovie=[aNotification object];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:MPMoviePlayerPlaybackDidFinishNotification
+                                                  object:theMovie];
+    NSLog(@"finished");  
+    [theMovie release];
+    [ self performSelectorOnMainThread:@selector(splashDone:)
+                            withObject:self 
+                         waitUntilDone:NO ];
+} 
+// play video
+- (void)video_play:(NSString*)filename
+{
+    NSString* file = [[NSBundle mainBundle] pathForResource:filename ofType:@"mp4"];
+    NSURL* url = [NSURL fileURLWithPath:file];
+    NSLog(@"Playing URL: %@", url);
+    [self playMovieAtURL:url];
+}
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -76,22 +143,40 @@
     [ self splashDoneMainThread:self ];
     
 }     
-     
+
 -(void) splashDoneMainThread:(id)obj
 {
-    [ self performSelectorOnMainThread:@selector(splashDone:)
-                         withObject:self 
-                         waitUntilDone:NO ];
+    [self video_play:@"DGNYE Logo Final 1"];
+    
 }
-     
+-(void)initmusic{
+    
+    //mp3 file  
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"sound" ofType:@"mp3"]; 
+    if (path) {   
+        player = [[AVAudioPlayer alloc]initWithContentsOfURL:[[NSURL alloc]initFileURLWithPath:path]error:nil];  
+        
+        [player prepareToPlay];  
+        player.numberOfLoops = -1;  
+        player.volume = 0.5f;   
+ 
+    } 
+    
+    
+}     
 -(void) splashDone:(id)obj
 {
+    
     djcAppDelegate *app = (djcAppDelegate *)
-        [ [ UIApplication sharedApplication ] delegate ];
+    [ [ UIApplication sharedApplication ] delegate ];
     [ app splashDone ];
+    //[self playMisic:@"DGNYE Logo Final 1"];
+    [self prepAudio]; 
+	// Start playback
+	[self.player play];
 }
 
-     
-     
+
+
 
 @end
